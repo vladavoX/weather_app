415e72ecb874d544f91665e11caf5cdc757283fb
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-01-25 11:23:10 +0100

    Fetch weather data from API

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from 'react'
+
 import CentralView from './components/CentralView'
 import LeftSidebar from './components/LeftSidebar'
 
 function App() {
+  const [weather, setWeather] = useState(null)
+  const fetchWeather = async () => {
+    return fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?q=London,GB&appid=${
+        import.meta.env.VITE_API_KEY
+      }&units=metric`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        setWeather(data)
+      })
+  }
+
+  useEffect(() => {
+    fetchWeather()
+  }, [])
+
   return (
     <div className='h-screen flex justify-center items-center bg-gradient-to-br from-orange-200 to-sky-200'>
       <LeftSidebar />
