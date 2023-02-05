57992d88bfcfbc57e793aa208b7bd0e13cd36539
Vladimir Aleksic <vlada.pzs@hotmail.com>
2023-02-05 17:30:30 +0100

    Add geolocation and add loading spinner

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,64 @@ import { useEffect, useState } from 'react'
 
 import CentralView from './components/CentralView'
 import LeftSidebar from './components/LeftSidebar'
+import LoadingSpinner from './components/LoadingSpinner'
 
 function App() {
   const [weather, setWeather] = useState(null)
   const [location, setLocation] = useState('')
   const [unit, setUnit] = useState<'metric' | 'imperial'>('metric')
   const [saveLocation, setSaveLocation] = useState('')
+  const [error, setError] = useState('')
+  const [loading, setLoading] = useState(false)
+
+  const geoLocation = () => {
+    try {
+      if (navigator.geolocation) {
+        setLoading(true)
+        navigator.geolocation.getCurrentPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords
+            fetch(
+              `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${
+                import.meta.env.VITE_API_KEY
+              }&units=${unit}`
+            )
+              .then((res) => res.json())
+              .then((data) => {
+                setWeather(data)
+                setLoading(false)
+              })
+          },
+          (error) => {
+            console.log(error)
+            setLoading(false)
+          }
+        )
+      }
+    } catch (error: any) {
+      setError(error.message)
+    }
+  }
 
   const fetchWeather = async () => {
-    const res = await fetch(
-      `https://api.openweathermap.org/data/2.5/forecast?q=${saveLocation}&appid=${
-        import.meta.env.VITE_API_KEY
-      }&units=${unit}`
-    )
-    const data = await res.json()
-    setWeather(data)
+    try {
+      setLoading(true)
+      const res = await fetch(
+        `https://api.openweathermap.org/data/2.5/forecast?q=${saveLocation}&appid=${
+          import.meta.env.VITE_API_KEY
+        }&units=${unit}`
+      )
+      const data = await res.json()
+      setWeather(data)
+      setLoading(false)
+    } catch (error: any) {
+      setError(error.message)
+    }
   }
+
+  useEffect(() => {
+    geoLocation()
+  }, [])
 
   useEffect(() => {
     if (location === '') return
@@ -31,17 +73,23 @@ function App() {
 
   return (
     <div className='h-screen flex justify-center items-center bg-gradient-to-br from-orange-200 to-sky-200'>
-      <LeftSidebar
-        location={location}
-        setLocation={setLocation}
-        fetchWeather={fetchWeather}
-      />
-      <CentralView
-        unit={unit}
-        setUnit={setUnit}
-        weather={weather}
-        setWeather={setWeather}
-      />
+      {error && <p className='text-red-500'>{error}</p>}
+      {loading && <LoadingSpinner />}
+      {!loading && !error && (
+        <>
+          <LeftSidebar
+            location={location}
+            setLocation={setLocation}
+            fetchWeather={fetchWeather}
+          />
+          <CentralView
+            unit={unit}
+            setUnit={setUnit}
+            weather={weather}
+            setWeather={setWeather}
+          />
+        </>
+      )}
     </div>
   )
 }
---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,11 @@
+const LoadingSpinner = () => {
+  return (
+    <div
+      className='w-12 h-12 rounded-full border-blue-400 border-t-4 border-r-4
+    animate-spin
+  '
+    ></div>
+  )
+}
+
+export default LoadingSpinner
